a6d76b924324f5c152f4b75f384ca83dd70b3b1d
jcgm_IA+D+i <proyectos@savsolutions.co>
2018-10-23 19:35:39 -0500

    Adición de fragmentos (clases y plantillas) de la versión de prueba para realización del proyecto

--- app/src/main/java/co/savsolutions/daltodesigner/MainActivity.kt
+++ app/src/main/java/co/savsolutions/daltodesigner/MainActivity.kt
@@ -1,10 +1,15 @@
 package co.savsolutions.daltodesigner
 
+import android.net.Uri
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import co.savsolutions.daltodesigner.ui.main.MainFragment
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(),
+    frgAreaDibujo.OnFragmentInteractionListener,
+    frgCargar_Diseno.OnFragmentInteractionListener,
+    frgFiltrosColores.OnFragmentInteractionListener,
+    frgCompartirDiseno.OnFragmentInteractionListener {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -14,6 +19,11 @@ class MainActivity : AppCompatActivity() {
                 .replace(R.id.container, MainFragment.newInstance())
                 .commitNow()
         }
+    }
+
+    override fun onFragmentInteraction(uri: Uri) {
+
+        // Prueba
     }
 
 }
